810cee8ad988ce9efc75e89b735e2f2f185efd24
Atatuber <atakan422005@gmail.com>
2025-06-21 13:06:54 +0200

    tekst fixes

--- src/components/Opening.jsx
+++ src/components/Opening.jsx
@@ -20,10 +20,10 @@ export function Opening() {
       <div className="absolute inset-0 flex items-center justify-center px-4">
         <div className="backdrop-blur-md rounded-xl p-6 md:p-10 border border-white border-opacity-30 shadow-xl max-w-4xl text-center">
           <h2 className="font-bold text-white text-3xl md:text-5xl drop-shadow-lg">
-            De lekkerste ontbijt, te vinden in Arnhem.
+            Het lekkerste ontbijt, te vinden in Arnhem.
           </h2>
           <p className="font-medium text-white text-base md:text-2xl mt-4 drop-shadow-lg">
-            Zin in een verrukkelijk ontbijt met krokante, versgebakken broodjes,
+            Zin in een verrukkelijk ontbijt met versgebakken broodjes,
             smaakvolle luxe gerechten en heerlijke drankjes? Bij ons bent u aan
             het juiste adres voor een perfecte start van de dag in hartje
             Arnhem.
